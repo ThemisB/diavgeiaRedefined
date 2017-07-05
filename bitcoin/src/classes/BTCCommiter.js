b59b34ed4470405876ef1143c8435d42c2c9f0af
Themis Beris <themis.n.beris@gmail.com>
2017-07-05 10:45:34 +0300

    Add Merkle Tree Construction

--- bitcoin/src/classes/BTCCommiter.js
+++ bitcoin/src/classes/BTCCommiter.js
@@ -3,6 +3,8 @@ const bcoin = require('bcoin').set(config.get('network'));
 const fs = require('co-fs');
 const crypto = require('crypto');
 const co = require('co');
+const merkle = require('merkle-tree-gen');
+const assert = require('assert');
 
 class BTCCommiter {
 
@@ -78,6 +80,55 @@ class BTCCommiter {
       return decisionsData.map(hashCalculate);
     });
   }
+
+  /**
+   * @typedef {Object.<hash, MerkleTreeNode>} MerkleTreeNodes
+   */
+
+  /**
+   * @typedef {Object} MerkleTreeNode
+   * @property {('leaf'|'node'|'root')} type - Role of the tree node
+   * @property {number} level - The height of the node
+   * @property {string} left - The left tree node of this tree node
+   * @property {string} right - The right tree node of this tree node
+   * @property {string} parent - The parent of the tree node
+   */
+
+  /**
+   * @typedef {Object} MerkleTree
+   * @property {string} root - The root of the Merkle Tree
+   * @property {string} hashalgo - The hash algorithm used on Merkle Tree Construction
+   * @property {number} leaves - The leaves of the tree. Equivalent to the number of decisions
+   * @property {number} levels - The height of the tree
+   * @property {Object} MerkleTreeNodes - Dynamic properties of the MerkleTree Object,
+   * corresponding to a node of the tree and keyed by their hash.
+   */
+
+  /**
+   * Create the Merkle Tree
+   * @param {DecisionsHash}
+   * @returns {MerkleTree}
+   */
+  constructMerkleTree(decisionsHash) {
+    var _btcCommiter = this;
+    const hashes = decisionsHash.map(decisionHash => decisionHash.hash);
+    const merkleTreeArgs = {
+      array: hashes,
+      hashalgo: config.get('hash_algorithm'),
+      hashlist: true
+    };
+    var tree = undefined;
+    merkle.fromArray(merkleTreeArgs, function(err, tr) {
+      if(!err){
+        tree = tr;
+      }
+      else {
+        assert.fail('Merkle Tree construction error', err);
+      }
+    });
+    return tree;
+  }
+
 }
 
 module.exports = BTCCommiter;
