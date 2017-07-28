bcf448eb102065603a4ad98cc73e636957d8df74
Themis Beris <themis.n.beris@gmail.com>
2017-07-28 12:23:01 +0300

    Use POST decisions array

--- n3-composer/components/Decision.vue
+++ n3-composer/components/Decision.vue
@@ -2,10 +2,10 @@
   <div class="col-xs-12 decision">
     <div class="row">
       <div class="col-xs-7">
-        <label :for="getDecisionLabel"><span class="badge">ΑΠΟΦΑΣΗ #{{number}}</span></label>
-        <textarea class="form-control" rows="2" :id="getDecisionLabel" :name="getDecisionLabel" :placeholder="getBadgePlaceholder"></textarea>
+        <label :for="getDecisionLabel"><span class="badge">ΑΠΟΦΑΣΗ #{{decisionNumber}}</span></label>
+        <textarea class="form-control" rows="2" :id="getDecisionLabel" :name="getDecisionName" :placeholder="getBadgePlaceholder"></textarea>
       </div>
-      <legislation-linking type="decision" v-bind:number="number"></legislation-linking>
+      <legislation-linking type="decision" v-bind:decisionNumber="decisionNumber"></legislation-linking>
     </div>
   </div>
 </template>
@@ -19,14 +19,17 @@ import autosize from 'autosize'
 
 export default {
   mixins: [ InputHandler ],
-  props: ['number'],
+  props: ['decisionNumber'],
   components: {LegislationLinking},
   computed: {
     getBadgePlaceholder: function () {
-      return 'Η ' + this.number + 'η Απόφαση του φορέα.'
+      return 'Η ' + this.decisionNumber + 'η Απόφαση του φορέα.'
     },
     getDecisionLabel: function () {
-      return 'decision-' + this.number
+      return 'decision-' + this.decisionNumber
+    },
+    getDecisionName: function () {
+      return 'decisions[' + this.decisionNumber + '][text]'
     }
   },
   mounted: function () {
