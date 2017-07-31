b5a367c568e41ca4c0bbbdca872d1a7f35b9279c
Themis Beris <themis.n.beris@gmail.com>
2017-07-31 15:20:08 +0300

    Placeholder change and add row class

--- n3-composer/components/Signer.vue
+++ n3-composer/components/Signer.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="row">
-  <div class="col-xs-6 col-xs-offset-3">
     <h5 class="text-center">Στοιχεία {{number}}ου υπογραφών</h5>
     <div class="col-xs-6">
       <label :for="getSignerNameLabel">Όνομα και επώνυμο</span></label>
@@ -8,11 +7,10 @@
     </div>
     <div class="col-xs-6">
       <label :for="getSignerJobLabel">Τίτλος Εργασίας</span></label>
-      <input type="text" class="form-control" :id="getSignerJobLabel" :name="getSignerJob" placeholder="π.χ. ΑΝΤΙΔΗΜΑΡΧΟΣ ΚΟΙΝΩΝΙΚΗΣ ΠΟΛΙΤΙΚΗΣ">
+      <input type="text" class="form-control" :id="getSignerJobLabel" :name="getSignerJob" placeholder="π.χ. ΑΝΤΙΔΗΜΑΡΧΟΣ ΠΕΙΡΑΙΑ">
     </div>
     <input type="hidden" :name="getSignerIndex" :value="number">
   </div>
-</div>
 </template>
 
 <script>
