b5e3a8f57311ef4d206b8b7fa58dffd2bd50f0f3
Themis Beris <themis.n.beris@gmail.com>
2017-07-31 09:59:54 +0300

    Add index to Signer

--- n3-composer/components/Signer.vue
+++ n3-composer/components/Signer.vue
@@ -3,13 +3,14 @@
   <div class="col-xs-6 col-xs-offset-3">
     <h5 class="text-center">Στοιχεία {{number}}ου υπογραφών</h5>
     <div class="col-xs-6">
-      <label :for="getSignerName">Όνομα και επώνυμο</span></label>
-      <input type="text" class="form-control" :id="getSignerName" :name="getSignerName">
+      <label :for="getSignerNameLabel">Όνομα και επώνυμο</span></label>
+      <input type="text" class="form-control" :id="getSignerNameLabel" :name="getSignerName">
     </div>
     <div class="col-xs-6">
-      <label :for="getSignerJob">Τίτλος Εργασίας</span></label>
-      <input type="text" class="form-control" :id="getSignerJob" :name="getSignerJob" placeholder="π.χ. ΑΝΤΙΔΗΜΑΡΧΟΣ ΚΟΙΝΩΝΙΚΗΣ ΠΟΛΙΤΙΚΗΣ">
+      <label :for="getSignerJobLabel">Τίτλος Εργασίας</span></label>
+      <input type="text" class="form-control" :id="getSignerJobLabel" :name="getSignerJob" placeholder="π.χ. ΑΝΤΙΔΗΜΑΡΧΟΣ ΚΟΙΝΩΝΙΚΗΣ ΠΟΛΙΤΙΚΗΣ">
     </div>
+    <input type="hidden" :name="getSignerIndex" :value="number">
   </div>
 </div>
 </template>
@@ -19,11 +20,20 @@
 module.exports = {
   props: ['number'],
   computed: {
-    getSignerName: function () {
+    getSignerNameLabel: function () {
       return 'signer-name-' + this.number
     },
-    getSignerJob: function () {
+    getSignerJobLabel: function () {
       return 'signer-job-' + this.number
+    },
+    getSignerName: function () {
+      return 'signer[' + this.number + '][name]'
+    },
+    getSignerJob: function () {
+      return 'signer[' + this.number + '][job]'
+    },
+    getSignerIndex: function () {
+      return 'signer[' + this.number + '][index]'
     }
   }
 }
