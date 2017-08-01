e9a651687aaa3e0468e2b0d78a98b55facbf67c4
Themis Beris <themis.n.beris@gmail.com>
2017-08-01 12:09:32 +0300

    Add Circular.vue

---
+++ n3-composer/components/specific-decision-fields/Circular.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="decisionSpecificFields row">
+    <div class="col-xs-4 col-xs-offset-4 text-center">
+      <label for="circular_number">Αριθμός Εγκυκλίου</label>
+      <input type="text" name="circular_number" class="form-control">
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
