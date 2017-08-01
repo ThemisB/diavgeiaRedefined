908f68e8bc55c1b7eb8a801a04becde00a5e57f8
Themis Beris <themis.n.beris@gmail.com>
2017-08-01 11:07:18 +0300

    Add Normative component

---
+++ n3-composer/components/specific-decision-fields/Normative.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="decisionSpecificFields row">
+    <div class="col-xs-4">
+      <normative-type></normative-type>
+    </div>
+    <div class="col-xs-4">
+      <label for="normative_number">Αριθμός Πράξης / Απόφασης</label>
+      <input type="text" name="normative_number" class="form-control">
+    </div>
+    <div class="col-xs-4">
+      <fek></fek>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import NormativeType from './common-properties/NormativeType.vue'
+import Fek from './common-properties/Fek.vue'
+
+export default {
+  components: {NormativeType, Fek}
+}
+
+</script>
