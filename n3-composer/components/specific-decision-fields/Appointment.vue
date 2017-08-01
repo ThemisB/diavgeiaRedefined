15f3f89df35802b4880b2ca6be130bfed5fed57c
Themis Beris <themis.n.beris@gmail.com>
2017-08-01 12:55:22 +0300

    Add Appointment component

---
+++ n3-composer/components/specific-decision-fields/Appointment.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="decisionSpecificFields row">
+    <div class="col-xs-4">
+      <label for="number_employees">Αριθμός Φυσικών Προσώπων</label>
+      <input type="text" name="number_employees" class="form-control">
+    </div>
+    <div class="col-xs-4">
+      <label for="appointment_employer_org">Φορέας που γίνεται ο διορισμός</label>
+      <input type="text" name="appointment_employer_org" class="form-control">
+    </div>
+    <div class="col-xs-4">
+      <fek></fek>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Fek from './common-properties/Fek.vue'
+
+export default {
+  components: {Fek}
+}
+
+</script>
