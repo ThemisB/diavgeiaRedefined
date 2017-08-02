e3cd092ea7d9420416e978bc944bcc1aca19d992
Themis Beris <themis.n.beris@gmail.com>
2017-08-02 15:55:51 +0300

    Add OtherDecisionsGeneral component

---
+++ n3-composer/components/specific-decision-fields/OtherDecisionsGeneral.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="decisionSpecificFields row">
+    <div class="col-xs-4">
+      <normative-type></normative-type>
+    </div>
+    <div class="col-xs-8">
+      <publish-via></publish-via>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import PublishVia from './common-properties/PublishVia.vue'
+import NormativeType from './common-properties/NormativeType.vue'
+
+export default {
+  components: {PublishVia, NormativeType}
+}
+</script>
