764886fc9fa1ed98502477930dc6cd42eced5318
Themis Beris <themis.n.beris@gmail.com>
2017-08-02 14:35:22 +0300

    Add LegislativeDecree component

---
+++ n3-composer/components/specific-decision-fields/LegislativeDecree.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="decisionSpecificFields row">
+    <div class="col-xs-4">
+      <label for="legislative_decree_number">Αριθμός Πράξης</label>
+      <input type="text" name="legislative_decree_number" class="form-control">
+    </div>
+    <div class="col-xs-8">
+      <fek></fek>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Fek from './common-properties/Fek.vue'
+
+export default {
+  components: {Fek}
+}
+
+</script>
