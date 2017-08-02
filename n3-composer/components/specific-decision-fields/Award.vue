bebaf127cd88bc5af833dcf6ce44c11f58f48236
Themis Beris <themis.n.beris@gmail.com>
2017-08-02 13:56:53 +0300

    Add Award component

---
+++ n3-composer/components/specific-decision-fields/Award.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="decisionSpecificFields row">
+    <div class="col-xs-10 col-xs-offset-1">
+     <expense-award></expense-award>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import ExpenseAward from './common-properties/ExpenseAward.vue'
+
+export default {
+  components: {ExpenseAward}
+}
+
+</script>
