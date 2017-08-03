dc972716fd33228d31d4656315720289bbc566e6
Themis Beris <themis.n.beris@gmail.com>
2017-08-03 18:45:56 +0300

    Add NumberEmployees component

---
+++ n3-composer/components/specific-decision-fields/common-properties/NumberEmployees.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <label for="number_employees">Αριθμός Φυσικών Προσώπων</label>
+    <input type="number" name="number_employees" class="form-control">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
