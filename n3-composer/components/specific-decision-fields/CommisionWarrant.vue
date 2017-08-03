6903a07179645e47d006a0256cb97f19aa651ffe
Themis Beris <themis.n.beris@gmail.com>
2017-08-03 16:32:50 +0300

    Add CommisionWarrant component

---
+++ n3-composer/components/specific-decision-fields/CommisionWarrant.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="decisionSpecificFields">
+  <div class="row">
+    <div class="col-xs-3">
+      <label for="primary_officer">Πρωτεύων Διατάκτης</label>
+      <input type="text" name="primary_officer" class="form-control">
+      </select>
+    </div>
+    <div class="col-xs-3">
+      <label for="secondary_officer">Δευτερεύων Διατάκτης</label>
+      <input type="text" name="secondary_officer" class="form-control">
+    </div>
+    <div class="col-xs-3">
+      <budget-category></budget-category>
+    </div>
+    <div class="col-xs-3">
+      <financial-year></financial-year>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-8 col-xs-offset-2">
+      <expense-with-kae></expense-with-kae>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+import FinancialYear from './common-properties/FinancialYear.vue'
+import BudgetCategory from './common-properties/BudgetCategory.vue'
+import ExpenseWithKae from './common-properties/ExpenseWithKae.vue'
+
+export default {
+  components: {FinancialYear, BudgetCategory, ExpenseWithKae}
+}
+</script>
