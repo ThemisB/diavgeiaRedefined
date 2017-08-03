907601d97dd69d90cca1d7b625a757ad8b582b6d
Themis Beris <themis.n.beris@gmail.com>
2017-08-03 13:23:37 +0300

    Add BudgetCategory component

---
+++ n3-composer/components/specific-decision-fields/common-properties/BudgetCategory.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <label for="budget_category">Κατηγορία Προϋπολογισμού</label>
+    <select class="selectpicker pickers" title="Κατηγορία Προϋπολογισμού" data-live-search="true" id="budget_category" name="budget_category" data-width="auto">
+      <option v-for="category in categories" :data-tokens="category.keywords" :value="category.text">{{category.text}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+
+import $ from 'jquery'
+
+export default {
+  data: () => {
+    return {
+      categories: [
+        {text: 'Ίδια Έσοδα', keywords: 'Ίδια Ιδια Έσοδα Εσοδα'},
+        {text: 'Πρόγραμμα Δημοσίων', keywords: 'Πρόγραμμα Προγραμμα Δημοσίων Δημοσιων'},
+        {text: 'Τακτικός Προϋπολογισμός', keywords: 'Τακτικός Τακτικος Προϋπολογισμός Προϋπολογισμος Προυπολογισμος'}
+      ]
+    }
+  },
+  mounted: function () {
+    $('#budget_category').selectpicker()
+  }
+}
+</script>
