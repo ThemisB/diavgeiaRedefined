316c4c3dd63fb2676fb103c05f67e1501c7f9dce
Themis Beris <themis.n.beris@gmail.com>
2017-08-03 12:14:37 +0300

    Add FinancialYear Component

---
+++ n3-composer/components/specific-decision-fields/common-properties/FinancialYear.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <label for="financial_year">Οικονομικό Έτος</label>
+    <input type="number" name="financial_year" class="form-control" minlength="4" maxlength="4" value="2017">
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
