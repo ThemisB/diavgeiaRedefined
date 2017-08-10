7bf72ea13e71256964a9500789e75bcb20471fc6
Themis Beris <themis.n.beris@gmail.com>
2017-08-10 13:22:37 +0300

    Support multiple KAEs

--- n3-composer/components/specific-decision-fields/common-properties/Kae.vue
+++ n3-composer/components/specific-decision-fields/common-properties/Kae.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    <label for="kae">Αριθμός ΚΑΕ</label>
-    <input type="text" name="kae" class="form-control">
+    <label for="kae" v-if="number === undefined">Αριθμός ΚΑΕ</label>
+    <input type="text" :name="getKaeName" class="form-control">
   </div>
 </template>
 
 <script>
 export default{
-
+  props: ['number'],
+  computed: {
+    getKaeName: function () {
+      if (this.number !== undefined) {
+        return 'expense[' + this.number + '][kae]'
+      }
+      return 'kae'
+    }
+  }
 }
 </script>
