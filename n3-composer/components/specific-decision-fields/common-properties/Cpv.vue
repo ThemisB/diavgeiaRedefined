cc36bcf94f06190bb1c69f9a3e2345167aef354b
Themis Beris <themis.n.beris@gmail.com>
2017-08-10 13:22:11 +0300

    Support multiple CPVs

--- n3-composer/components/specific-decision-fields/common-properties/Cpv.vue
+++ n3-composer/components/specific-decision-fields/common-properties/Cpv.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    <label for="cpv">CPV</label>
-    <input type="text" name="cpv" class="form-control">
+    <label for="cpv" v-if="number === undefined">CPV</label>
+    <input type="text" :name="getCPVName" class="form-control">
   </div>
 </template>
 
 <script>
 export default {
-
+  props: ['number'],
+  computed: {
+    getCPVName: function () {
+      if (this.number !== undefined) {
+        return 'expense[' + this.number + '][cpv]'
+      }
+      return 'cpv'
+    }
+  }
 }
 </script>
