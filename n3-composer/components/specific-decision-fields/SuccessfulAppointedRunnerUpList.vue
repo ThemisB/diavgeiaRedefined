be09ecd8743429270a58a751acb6f327f4e37d93
Themis Beris <themis.n.beris@gmail.com>
2017-08-11 10:52:20 +0300

    Add SuccessfulAppointedRunnerUpList component

---
+++ n3-composer/components/specific-decision-fields/SuccessfulAppointedRunnerUpList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="decisionSpecificFields row">
+    <div class="col-xs-6 col-xs-offset-3">
+      <label for="has_related_occupation_invitation">Σχετική Προκήρυξη Πλήρωσης Θέσεων</label>
+      <input type="text" class="form-control" name="has_related_occupation_invitation" placeholder="Ο ΑΔΑ της σχετικής Προκήρυξης Πλήρωσης Θέσεων">
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
