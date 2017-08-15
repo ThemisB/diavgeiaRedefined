dc99cde86401bb567c6e15f3385d1c732d1f26c7
Themis Beris <themis.n.beris@gmail.com>
2017-08-15 12:26:43 +0300

    Add success message on form submission

--- n3-composer/pages/index.vue
+++ n3-composer/pages/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="container">
+    <div class="alert alert-success alert-dismissible successfulSubmission" role="alert" v-if="success">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      Η απόφαση έχει αναρτηθεί στην Διαύγεια και στο SPARQL endpoint
+    </div>
+
     <h1 class="text-center decisionCreation dvgColor">Δημιουργία Απόφασης Φορέα</h1>
     <form class="panel" action="/api/createDecision" method="post">
       <government-institution-info></government-institution-info>
@@ -20,6 +25,36 @@ import GovernmentInstitutionInfo from '../components/GovernmentInstitutionInfo.v
 import DecisionsComposer from '../components/DecisionsComposer.vue'
 
 export default {
-  components: {GovernmentInstitutionInfo, DecisionsComposer}
+  components: {GovernmentInstitutionInfo, DecisionsComposer},
+  mounted: function () {
+    // On successful form submission API returns URL:?success
+    let isSuccess = (function () {
+      let param = 'success'
+      let url = window.location.href
+      param = param.replace(/[[\]]/g, '\\$&')
+      let regex = new RegExp('[?&]' + param + '(=([^&#]*)|&|#|$)')
+      var results = regex.exec(url)
+      if (results && !results[2]) {
+        return true
+      }
+      return false
+    })()
+
+    if (isSuccess) {
+      this.success = true
+      history.pushState(null, '', window.location.href.substring(0, window.location.href.indexOf('?')))
+    }
+  },
+  data: function () {
+    return {
+      success: false
+    }
+  }
 }
 </script>
+
+<style scoped>
+.successfulSubmission {
+  margin-top: 1.4em;
+}
+</style>
