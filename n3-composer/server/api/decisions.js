eaa3fa7e5224932ccbcefdd26db81b384b3155fc
Themis Beris <themis.n.beris@gmail.com>
2017-08-15 12:33:22 +0300

    Return success param on successful decision upload

--- n3-composer/server/api/decisions.js
+++ n3-composer/server/api/decisions.js
@@ -9,6 +9,7 @@ router.post('/createDecision', function(req, res) {
   // TODO These 4 fields (IUN, Version, unitIds, organizationId) should be set
   // by the current implementation of Diavgeia
   new Decision(req.body,'60Β3ΩΡΙ-ΒΝ3','b4ae1411-f81d-437b-9c63-a8b7d4ed866b',['6105'],'93302').generateN3()
+  res.redirect('/?success');
 })
 
 export default router
