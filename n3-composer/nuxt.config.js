f822ef951e44aad3b2ce6e090adce28735a9dfa0
Themis Beris <themis.n.beris@gmail.com>
2017-10-12 13:27:24 +0300

    Change configurations for new nuxt version

--- n3-composer/nuxt.config.js
+++ n3-composer/nuxt.config.js
@@ -9,21 +9,22 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { 'http-equiv' : 'X-UA-Compatible', content: 'IE=edge'},
-      { name: 'theme-color', content: '#ffffff'},
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+      { name: 'theme-color', content: '#ffffff' }
     ],
     link: [
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png'},
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png'},
-      { rel: 'icon', type: 'image/png', size: '16x16', href: '/favicon-16x16.png'},
-      { rel: 'manifest', href: '/manifest.json'},
-      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5'},
-      { type: 'text/css', rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'}
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/manifest.json' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+      { type: 'text/css', rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.3/css/bulma.min.css' },
+      { type: 'text/css', rel: 'stylesheet', href: 'https://unpkg.com/vue-multiselect@2.0.0/dist/vue-multiselect.min.css' },
+      { type: 'text/css', rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' }
     ]
   },
   css: [
-    '~assets/css/style.css',
-    'bootstrap-select/dist/css/bootstrap-select.min.css'
+    '~assets/css/style.css'
   ],
   /*
   ** Customize the progress-bar color
@@ -36,7 +37,7 @@ module.exports = {
     /*
     ** Run ESLINT on save
     */
-    vendor: ['jquery','bootstrap', 'autosize', 'bootstrap-select', 'axios'],
+    vendor: ['jquery', 'autosize', 'axios'],
     plugins: [
       new webpack.ProvidePlugin({
         jQuery: 'jquery',
