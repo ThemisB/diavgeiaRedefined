ba3ff1d399d0177e054c4f23b45e0c9923e2f765
Themis Beris <themis.n.beris@gmail.com>
2017-10-13 14:48:03 +0300

    BOOTSTRAP_TO_BULMA: FinancialYear

--- n3-composer/components/specific-decision-fields/common-properties/FinancialYear.vue
+++ n3-composer/components/specific-decision-fields/common-properties/FinancialYear.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <label for="financial_year">Οικονομικό Έτος</label>
-    <input type="number" name="financial_year" class="form-control" minlength="4" maxlength="4" value="2017">
+    <input type="number" name="financial_year" minlength="4" maxlength="4" value="2017" class="input">
   </div>
 </template>
 
