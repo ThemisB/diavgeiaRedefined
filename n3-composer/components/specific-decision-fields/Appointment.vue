c6f9c93b89b68fa808ea075ac160a56061f757f4
Themis Beris <themis.n.beris@gmail.com>
2017-10-13 12:03:55 +0300

    BOOTSTRAP_TO_BULMA: Appointment

--- n3-composer/components/specific-decision-fields/Appointment.vue
+++ n3-composer/components/specific-decision-fields/Appointment.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="decisionSpecificFields row">
-    <div class="col-xs-4">
+  <div class="decisionSpecificFields columns">
+    <div class="column">
       <label for="number_employees">Αριθμός Φυσικών Προσώπων</label>
-      <input type="text" name="number_employees" class="form-control">
+      <input type="text" name="number_employees" class="input">
     </div>
-    <div class="col-xs-4">
+    <div class="column">
       <label for="appointment_employer_org">Φορέας που γίνεται ο διορισμός</label>
-      <input type="text" name="appointment_employer_org" class="form-control">
+      <input type="text" name="appointment_employer_org" class="input">
     </div>
-    <div class="col-xs-4">
+    <div class="column">
       <fek></fek>
     </div>
   </div>
