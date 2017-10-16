6f89f4aaa9bf1fd7a438c02f639f7d33a98fdab9
Themis Beris <themis.n.beris@gmail.com>
2017-10-16 14:47:08 +0300

    BOOTSTRAP_TO_BULMA: BudgetApproval

--- n3-composer/components/specific-decision-fields/BudgetApproval.vue
+++ n3-composer/components/specific-decision-fields/BudgetApproval.vue
@@ -1,25 +1,30 @@
 <template>
-  <div class="decisionSpecificFields row">
-    <div class="col-xs-4">
-      <label for="budget_type">Είδος Προϋπολογισμού</label>
-      <select class="selectpicker pickers" title="Είδος Προϋπολογισμού" data-live-search="true" id="budget_type" name="budget_type" data-width="auto">
-        <option v-for="budgetType in budgetTypes" :data-tokens="budgetType.keywords" :value="budgetType.text">{{budgetType.text}}</option>
-      </select>
-    </div>
-    <div class="col-xs-4">
-      <budget-category></budget-category>
-    </div>
-    <div class="col-xs-4">
-      <financial-year></financial-year>
-    </div>
-    <div class="row balanceAccountPadding">
-      <div class="col-xs-4 col-xs-offset-1">
-        <label for="is_budget_approval_for_org">Έγκριση Ισολογισμού/Απολογισμού τρίτου Φορέα</label>
-        <input type="checkbox" name="is_budget_approval_for_org" id="is_budget_approval_for_org" v-model="checked">
+  <div class="decisionSpecificFields" style="padding-bottom:2em;">
+    <div class="columns">
+      <div class="column">
+        <label for="budget_type">Είδος Προϋπολογισμού</label>
+        <multiselect id="budget_type" v-model="selected" :options="budgetTypes" select-label="" selected-label="" deselect-label="" placeholder="">
+        <span slot="noResult">Δεν βρέθηκε το είδος προϋπολογισμού</span>
+        </multiselect>
+        <!-- Hack for vue-multiselect, based on this issue https://github.com/monterail/vue-multiselect/issues/299 -->
+        <input type="hidden" name="budget_type" :value="selected">
       </div>
-      <div v-if="checked" class="col-xs-4 col-xs-offset-1">
+      <div class="column">
+        <budget-category></budget-category>
+      </div>
+      <div class="column">
+        <financial-year></financial-year>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column is-one-third">
+        <div class="field">
+          <label for="is_budget_approval_for_org" class="checkbox">Έγκριση Ισολογισμού/Απολογισμού τρίτου Φορέα<input type="checkbox" name="is_budget_approval_for_org" id="is_budget_approval_for_org" v-model="checked"></label>
+        </div>
+      </div>
+      <div v-if="checked" class="column is-two-thirds has-text-centered">
         <label for="has_related_institution">Φορέας που αφορά</label>
-        <input name="has_related_institution" class="form-control">
+        <input name="has_related_institution" class="input">
       </div>
     </div>
   </div>
@@ -27,23 +32,18 @@
 
 <script>
 
-import $ from 'jquery'
 import FinancialYear from './common-properties/FinancialYear.vue'
 import BudgetCategory from './common-properties/BudgetCategory.vue'
+import Multiselect from 'vue-multiselect'
 
 export default {
   data: () => {
     return {
-      budgetTypes: [
-        {text: 'Κρατικός', keywords: 'Κρατικός Κρατικος'},
-        {text: 'Φορέα', keywords: 'Φορέα Φορεα'}
-      ],
-      checked: ''
+      budgetTypes: ['Κρατικός', 'Φορέα'],
+      checked: '',
+      selected: ''
     }
   },
-  mounted: function () {
-    $('#budget_type').selectpicker()
-  },
-  components: {FinancialYear, BudgetCategory}
+  components: {FinancialYear, BudgetCategory, Multiselect}
 }
 </script>
