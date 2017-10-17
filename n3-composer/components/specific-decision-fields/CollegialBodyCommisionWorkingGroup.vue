ffe0a10aa972372b0f900e4573301d54dff0ab82
Themis Beris <themis.n.beris@gmail.com>
2017-10-17 13:23:51 +0300

    BOOTSTRAP_TO_BULMA: CollegialBodyCommisionWorkingGroup

--- n3-composer/components/specific-decision-fields/CollegialBodyCommisionWorkingGroup.vue
+++ n3-composer/components/specific-decision-fields/CollegialBodyCommisionWorkingGroup.vue
@@ -1,28 +1,36 @@
 <template>
-  <div class="decisionSpecificFields row">
-    <div class="col-xs-4">
-      <label for="collegial_body_decision_type">Είδος Πράξης</label>
-      <select class="selectpicker pickers" title="Είδος Πράξης" data-live-search="true" id="collegial_body_decision_type" name="collegial_body_decision_type" data-width="auto" v-model="selected">
-        <option v-for="decisonType in decisionTypes" :data-tokens="decisonType.keywords" :value="decisonType.text">{{decisonType.text}}</option>
-      </select>
-    </div>
-    <div class="col-xs-4">
-      <label for="collegial_body_party_type">Τύπος Οργάνου</label>
-      <select class="selectpicker pickers" title="Τύπος Οργάνου" data-live-search="true" id="collegial_body_party_type" name="collegial_body_party_type" data-width="auto">
-        <option v-for="partyType in partyTypes" :data-tokens="partyType.keywords" :value="partyType.text">{{partyType.text}}</option>
-      </select>
-    </div>
-    <div v-if="selected === 'Καθορισμός Αμοιβής - Αποζημίωσης'" class="col-xs-4">
-      <div class="col-xs-6">
-        <label for="collegial_body_refund">Ποσό</label>
-        <input type="number" name="collegial_body_refund" class="form-control">
+  <div class="decisionSpecificFields">
+    <div class="columns">
+      <div class="column">
+        <label for="collegial_body_decision_type">Τύπος Πράξης</label>
+        <multiselect v-model="selectedDecisionType" :options="decisionTypes" select-label="" selected-label="" deselect-label="" placeholder="">
+        <span slot="noResult">Δεν βρέθηκε ο τύπος πράξης</span>
+        </multiselect>
+        <!-- Hack for vue-multiselect, based on this issue https://github.com/monterail/vue-multiselect/issues/299 -->
+        <input type="hidden" name="collegial_body_decision_type" :value="selectedDecisionType">
       </div>
-      <div class="col-xs-6">
-        <currencies></currencies>
+      <div class="column">
+        <label for="collegial_body_party_type">Τύπος Οργάνου</label>
+        <multiselect v-model="selectedPartyType" :options="partyTypes" select-label="" selected-label="" deselect-label="" placeholder="">
+        <span slot="noResult">Δεν βρέθηκε ο τύπος οργάνου</span>
+        </multiselect>
+        <!-- Hack for vue-multiselect, based on this issue https://github.com/monterail/vue-multiselect/issues/299 -->
+        <input type="hidden" name="collegial_body_party_type" :value="selectedPartyType">
+      </div>
+      <div v-if="selectedDecisionType === 'Καθορισμός Αμοιβής - Αποζημίωσης'" class="column">
+        <div class="columns">
+          <div class="column">
+            <label for="collegial_body_refund">Ποσό</label>
+            <input type="number" name="collegial_body_refund" class="input" required="required">
+          </div>
+          <div class="column">
+            <currencies></currencies>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="row">
-      <div class="col-xs-8 col-xs-offset-2">
+    <div class="columns">
+      <div class="column">
         <fek></fek>
       </div>
     </div>
@@ -33,32 +41,17 @@
 
 import Fek from './common-properties/Fek.vue'
 import Currencies from './common-properties/Currencies.vue'
-import $ from 'jquery'
+import Multiselect from 'vue-multiselect'
 
 export default {
   data: () => {
     return {
-      decisionTypes: [
-        {text: 'Αποδοχή Παραίτησης Μέλους', keywords: 'Αποδοχή Αποδοχη Παραίτησης Παραιτησης Μέλους Μελους'},
-        {text: 'Καθορισμός Αμοιβής - Αποζημίωσης', keywords: 'Καθορισμός Καθορισμος Αμοιβής Αμοιβης Αποζημίωσης Αποζημιωσης'},
-        {text: 'Παύση - Αντικατάσταση μέλους', keywords: 'Παύση Παυση Αντικατάσταση Αντικατασταση Μέλους Μελους'},
-        {text: 'Συγκρότηση', keywords: 'Συγκρότηση Συγκροτηση'}
-      ],
-      partyTypes: [
-        {text: 'Όργανο Γνωμοδοτικής Αρμοδιότητας', keywords: 'Όργανο Οργανο Γνωμοδοτικής Γνωμοδοτικης Αρμοδιότητας Αρμοδιοτητας'},
-        {text: 'Όργανο άλλης αρμοδιότητας', keywords: 'Όργανο Οργανο Αρμοδιότητας Αρμοδιοτητας Άλλης Αλλης'},
-        {text: 'Επιτροπή', keywords: 'Επιτροπή Επιτροπη'},
-        {text: 'Ομάδα έργου', keywords: 'Ομάδα Ομαδα Έργου Εργου'},
-        {text: 'Ομάδα εργασίας', keywords: 'Ομάδα Ομαδα Εργασίας Εργασιας'},
-        {text: 'Συλλογικό όργανο Διοίκησης', keywords: 'Συλλογικό Συλλογικο Όργανο Οργανο Διοίκησης Διοικησης'}
-      ],
-      selected: ''
+      decisionTypes: ['Αποδοχή Παραίτησης Μέλους', 'Καθορισμός Αμοιβής - Αποζημίωσης', 'Παύση - Αντικατάσταση μέλους', 'Συγκρότηση'],
+      partyTypes: ['Όργανο Γνωμοδοτικής Αρμοδιότητας', 'Όργανο άλλης αρμοδιότητας', 'Επιτροπή', 'Ομάδα έργου', 'Ομάδα εργασίας', 'Συλλογικό όργανο Διοίκησης'],
+      selectedDecisionType: '',
+      selectedPartyType: ''
     }
   },
-  mounted: function () {
-    $('#collegial_body_decision_type').selectpicker()
-    $('#collegial_body_party_type').selectpicker()
-  },
-  components: {Fek, Currencies}
+  components: {Fek, Currencies, Multiselect}
 }
 </script>
