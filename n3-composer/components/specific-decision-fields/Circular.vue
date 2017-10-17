0ea600a38a55001c7c9ddac3e8e367e00bdc79a2
Themis Beris <themis.n.beris@gmail.com>
2017-10-17 12:26:53 +0300

    BOOTSTRAP_TO_BULMA: Circular

--- n3-composer/components/specific-decision-fields/Circular.vue
+++ n3-composer/components/specific-decision-fields/Circular.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="decisionSpecificFields row">
-    <div class="col-xs-4 col-xs-offset-4 text-center">
-      <label for="circular_number">Αριθμός Εγκυκλίου</label>
-      <input type="text" name="circular_number" class="form-control">
+  <div class="decisionSpecificFields columns is-centered" style="margin-bottom:1em;">
+    <div class="column is-half">
+      <label for="circular_number" class="label" required>Αριθμός Εγκυκλίου</label>
+      <input type="text" name="circular_number" class="input">
     </div>
   </div>
 </template>
