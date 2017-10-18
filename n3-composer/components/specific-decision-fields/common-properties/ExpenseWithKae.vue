04eb483dd6d3585918c885b4bbedbad32756c916
Themis Beris <themis.n.beris@gmail.com>
2017-10-18 11:10:38 +0300

    BOOTSTRAP_TO_BULMA: ExpenseWithKae

--- n3-composer/components/specific-decision-fields/common-properties/ExpenseWithKae.vue
+++ n3-composer/components/specific-decision-fields/common-properties/ExpenseWithKae.vue
@@ -1,32 +1,35 @@
 <template>
-<div class="text-center">
-  <table class="expensesTable table table-striped table-bordered expensesWithKae">
-    <thead>
-      <tr>
-        <th>ΑΡΙΘΜΟΣ ΚΑΕ</th>
-        <th>ΠΟΣΟ ΜΕ ΦΠΑ</th>
-        <th>ΠΡΟΣΘΗΚΗ</th>
-      </tr>
-    </thead>
-    <tbody class="text-center">
-      <tr v-for="expense in expensesArray" :key="expense.id = expense">
-        <td><input type="text" :name="getKae(expense)" class="form-control"></td>
-        <td>
-          <div class="col-xs-6">
-          <label :for="getExpense(expense)">Ποσό</label>
-          <input type="text" :name="getExpense(expense)">
-        </div>
-          <div class="col-xs-6">
-            <currencies :number="expense"></currencies>
-          </div>
-        </td>
-        <input type="hidden" :value="expense" :name="getExpenseIndex(expense)">
-
-        <td v-if="expense === 1"><button type="button" class="btn btn-default" v-on:click="incrementExpenses"><span>Προσθήκη {{nextExpense}}ου Ποσού</span></button></td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+  <div class="column" style="padding-bottom:2.5em;">
+    <table class="table is-striped is-bordered has-text-centered expenses-table" style="margin:0 auto;">
+      <thead>
+        <tr>
+          <th>ΑΡΙΘΜΟΣ ΚΑΕ</th>
+          <th>ΠΟΣΟ ΜΕ ΦΠΑ</th>
+          <th>ΠΡΟΣΘΗΚΗ</th>
+        </tr>
+      </thead>
+      <tbody class="has-text-centered">
+        <tr v-for="expense in expensesArray" :key="getKey('expense', expense)">
+          <td>
+            <input type="text" :name="getKae(expense)" class="input">
+          </td>
+          <td>
+            <div class="columns">
+              <div class="column">
+                <label :for="getExpense(expense)">Ποσό</label>
+                <input type="text" :name="getExpense(expense)" class="input">
+              </div>
+              <div class="column">
+                <currencies :number="expense"></currencies>
+              </div>
+            </div>
+          </td>
+          <input type="hidden" :value="expense" :name="getExpenseIndex(expense)">
+          <td v-if="expense === 1"><button type="button" class="button is-info is-outlined" v-on:click="incrementExpenses"><span>Προσθήκη {{nextExpense}}ου Ποσού</span></button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -56,6 +59,9 @@ export default {
     },
     getExpense: function (expenseNumber) {
       return 'expense[' + expenseNumber + '][expense_amount]'
+    },
+    getKey: function (keyName, index) {
+      return keyName + '_' + index
     }
   },
   data: function () {
