5cc7ec7f778aebf2d06fe83dcd471f97cb87049a
Themis Beris <themis.n.beris@gmail.com>
2017-10-19 13:16:37 +0300

    BOOTSTRAP_TO_BULMA: DonationGrant

--- n3-composer/components/specific-decision-fields/DonationGrant.vue
+++ n3-composer/components/specific-decision-fields/DonationGrant.vue
@@ -1,50 +1,36 @@
 <template>
 <div class="decisionSpecificFields">
-  <div class="row">
-    <div class="col-xs-3">
+  <div class="columns">
+    <div class="column">
       <label for="donation_type">Είδος Πράξης</label>
-      <select class="selectpicker pickers" title="Είδος Πράξης" data-live-search="true" id="donation_type" name="donation_type" data-width="auto">
-        <option v-for="donationType in donationTypes" :data-tokens="donationType.keywords" :value="donationType.text">{{donationType.text}}</option>
-      </select>
+      <multiselect v-model="selected" :options="donationTypes" select-label="" selected-label="" deselect-label="" placeholder="">
+      <span slot="noResult">Δεν βρέθηκε το είδος πράξης</span>
+      </multiselect>
+      <!-- Hack for vue-multiselect, based on this issue https://github.com/monterail/vue-multiselect/issues/299 -->
+      <input type="hidden" name="donation_type" :value="selected">
     </div>
-    <div class="col-xs-3">
-      <label for="expense_amount">Ποσό</label>
-      <input type="number" class="form-control" name="expense_amount">
-    </div>
-    <div class="col-xs-3">
-      <currencies></currencies>
-    </div>
-    <div class="col-xs-3">
+    <div class="column">
       <kae></kae>
     </div>
   </div>
-  <div class="row">
-    <expense-donation></expense-donation>
-  </div>
+  <expense-donation></expense-donation>
 </div>
 </template>
 
 <script>
 
 import Kae from './common-properties/Kae.vue'
-import Currencies from './common-properties/Currencies.vue'
 import ExpenseDonation from './common-properties/ExpenseDonation'
-import $ from 'jquery'
+import Multiselect from 'vue-multiselect'
+
 
 export default {
   data: () => {
     return {
-      donationTypes: [
-        {text: 'Αποδοχή Δωρεάς', keywords: 'Αποδοχή Αποδοχη Δωρέας Δωρεας'},
-        {text: 'Δωρεά(προς τρίτους)', keywords: 'Δωρεά Δωρεα Τρίτους Τριτους Προς'},
-        {text: 'Επιχορήγηση', keywords: 'Επιχορήγηση Επιχορηγηση'},
-        {text: 'Σύμβαση Πολιστικής Χορηγίας', keywords: 'Σύμβαση Συμβαση Πολιτιστικής Πολιτιστικης Χορηγίας Χορηγιας'}
-      ]
+      donationTypes: ['Αποδοχή Δωρεάς', 'Δωρεά(προς τρίτους)', 'Επιχορήγηση', 'Σύμβαση Πολιστικής Χορηγίας'],
+      selected: ''
     }
   },
-  mounted: function () {
-    $('#donation_type').selectpicker()
-  },
-  components: {Kae, Currencies, ExpenseDonation}
+  components: {Kae, ExpenseDonation, Multiselect}
 }
 </script>
