9484ab79a4d97ca318bc70b3c01156b61e65286d
Themis Beris <themis.n.beris@gmail.com>
2017-10-19 13:56:24 +0300

    BOOTSTRAP_TO_BULMA: ExpenditureApproval

--- n3-composer/components/specific-decision-fields/ExpenditureApproval.vue
+++ n3-composer/components/specific-decision-fields/ExpenditureApproval.vue
@@ -1,32 +1,14 @@
 <template>
 <div class="decisionSpecificFields">
-  <div class="row">
-    <expense-expenditure-approval></expense-expenditure-approval>
-  </div>
+  <expense-expenditure-approval></expense-expenditure-approval>
 </div>
 </template>
 
 <script>
 
-import Kae from './common-properties/Kae.vue'
-import Currencies from './common-properties/Currencies.vue'
 import ExpenseExpenditureApproval from './common-properties/ExpenseExpenditureApproval.vue'
-import $ from 'jquery'
 
 export default {
-  data: () => {
-    return {
-      donationTypes: [
-        {text: 'Αποδοχή Δωρεάς', keywords: 'Αποδοχή Αποδοχη Δωρέας Δωρεας'},
-        {text: 'Δωρεά(προς τρίτους)', keywords: 'Δωρεά Δωρεα Τρίτους Τριτους Προς'},
-        {text: 'Επιχορήγηση', keywords: 'Επιχορήγηση Επιχορηγηση'},
-        {text: 'Σύμβαση Πολιστικής Χορηγίας', keywords: 'Σύμβαση Συμβαση Πολιτιστικής Πολιτιστικης Χορηγίας Χορηγιας'}
-      ]
-    }
-  },
-  mounted: function () {
-    $('#donation_type').selectpicker()
-  },
-  components: {Kae, Currencies, ExpenseExpenditureApproval}
+  components: {ExpenseExpenditureApproval}
 }
 </script>
