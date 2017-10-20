8427944d2de8a4301f49c92c9da5baaaf2e1db3e
Themis Beris <themis.n.beris@gmail.com>
2017-10-20 12:39:20 +0300

    BOOTSTRAP_TO_BULMA: Normative

--- n3-composer/components/specific-decision-fields/Normative.vue
+++ n3-composer/components/specific-decision-fields/Normative.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="decisionSpecificFields row">
-    <div class="col-xs-4">
+  <div class="decisionSpecificFields columns">
+    <div class="column">
       <normative-type></normative-type>
     </div>
-    <div class="col-xs-4">
+    <div class="column">
       <label for="normative_number">Αριθμός Πράξης / Απόφασης</label>
-      <input type="text" name="normative_number" class="form-control">
+      <input type="text" name="normative_number" class="input">
     </div>
-    <div class="col-xs-4">
+    <div class="column">
       <fek></fek>
     </div>
   </div>
