149ea64e694707343d097ced7274920df4fcf0b0
Themis Beris <themis.n.beris@gmail.com>
2017-10-23 12:50:02 +0300

    BOOTSTRAP_TO_BULMA: SuccessfulAppointedRunnerUpList

--- n3-composer/components/specific-decision-fields/SuccessfulAppointedRunnerUpList.vue
+++ n3-composer/components/specific-decision-fields/SuccessfulAppointedRunnerUpList.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="decisionSpecificFields row">
-    <div class="col-xs-6 col-xs-offset-3">
+  <div class="columns is-centered" style="padding-bottom:1.3em">
+    <div class="column is-half">
       <label for="has_related_occupation_invitation">Σχετική Προκήρυξη Πλήρωσης Θέσεων</label>
-      <input type="text" class="form-control" name="has_related_occupation_invitation" placeholder="Ο ΑΔΑ της σχετικής Προκήρυξης Πλήρωσης Θέσεων">
+      <input type="text" class="input" name="has_related_occupation_invitation" placeholder="Ο ΑΔΑ της σχετικής Προκήρυξης Πλήρωσης Θέσεων">
     </div>
   </div>
 </template>
