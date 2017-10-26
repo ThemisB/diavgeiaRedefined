3d421d0d1af66d13acc05029f23913066bfda838
Themis Beris <themis.n.beris@gmail.com>
2017-10-26 10:12:21 +0300

    Update the :name of Kae for the case of PaymentFinalisation

--- n3-composer/components/specific-decision-fields/common-properties/Kae.vue
+++ n3-composer/components/specific-decision-fields/common-properties/Kae.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
-    <label for="kae" v-if="number === undefined">Αριθμός ΚΑΕ</label>
+    <label :for="getKaeName" v-if="number === undefined">Αριθμός ΚΑΕ</label>
+    <label :for="getKaeName" v-if="subExpenseNumber !== undefined">Κωδικός ΚΑΕ</label>
     <input type="text" :name="getKaeName" class="input">
   </div>
 </template>
 
 <script>
 export default{
-  props: ['number'],
+  props: ['number', 'subExpenseNumber'],
   computed: {
     getKaeName: function () {
       if (this.number !== undefined) {
+        if (this.subExpenseNumber !== undefined) {
+          return 'expense[' + this.number + '][withKaeSubExpense][' + this.subExpenseNumber + '][kae]'
+        }
         return 'expense[' + this.number + '][kae]'
       }
       return 'kae'
