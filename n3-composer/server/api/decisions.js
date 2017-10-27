d655c4031433846623c83e0fa3c346f5647d7490
Themis Beris <themis.n.beris@gmail.com>
2017-10-27 13:51:11 +0300

    Add error handling on api

--- n3-composer/server/api/decisions.js
+++ n3-composer/server/api/decisions.js
@@ -5,11 +5,14 @@ var router = Router()
 
 router.use(bodyParser.urlencoded({ extended: true }))
 
-router.post('/createDecision', function(req, res) {
+router.post('/createDecision', function (req, res) {
   // TODO These 4 fields (IUN, Version, unitIds, organizationId) should be set
   // by the current implementation of Diavgeia
-  new Decision(req.body,'60Β3ΩΡΙ-ΒΝ3','b4ae1411-f81d-437b-9c63-a8b7d4ed866b',['6105'],'93302').generateN3()
-  res.redirect('/?success');
+  let err = new Decision(req.body, '60Β3ΩΡΙ-ΒΝ3', 'b4ae1411-f81d-437b-9c63-a8b7d4ed866b', ['6105'], '93302').generateN3()
+  if (err) {
+    res.redirect('/?error')
+  }
+  res.redirect('/?success')
 })
 
 export default router
