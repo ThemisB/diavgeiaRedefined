69dcb64bf727a34675439cba21962494be58e384
Themis Beris <themis.n.beris@gmail.com>
2017-11-15 19:49:59 +0200

    Prepend http to visualizer link and display properly 3 decisions per row

--- n3-composer/components/navigation/Decision.vue
+++ n3-composer/components/navigation/Decision.vue
@@ -7,11 +7,12 @@
             <div style="padding-bottom:0.3em"><span class="tag">ΑΔΑ</span>:&nbsp;{{decision.iun}} </div>
             <div style="padding-bottom:0.3em"><span class="tag"> Version</span>:&nbsp;{{decision.version}} </div>
             <div><span class="tag">Ημερομηνία ανάρτησης</span>:&nbsp; <time>{{decision.date | formatDate}}</time></div>
+            <div class="has-text-centered" style="padding-top:1em" v-if="decision.txIndex || decision.txIndex === 0"><i class="fa fa-btc btcColor"></i><span style="font-size:1.1em"><b>-secured</b></span></div>
           </p>
       </div>
       <footer class="card-footer">
         <p class="card-footer-item">
-          <span><a :href="visualiseLink()" target="_blank">Oπτικοποίηση</a></span>
+          <a :href="visualiseLink()" target="_blank">Oπτικοποίηση</a>
         </p>
         <p class="card-footer-item">
           <span><a :href="getDownloadLink()">Κατεβάστε την απόφαση</a></span>
@@ -29,7 +30,7 @@ export default {
   props: ['decision'],
   methods: {
     visualiseLink: function () {
-      return 'localhost:3333/visualize?iun=' + this.decision.iun + '&version=' + this.decision.version
+      return 'http://localhost:3333/visualize?iun=' + this.decision.iun + '&version=' + this.decision.version
     },
     getDownloadLink: function () {
       return '/api/downloadDecision?iun=' + this.decision.iun + '&version=' + this.decision.version
