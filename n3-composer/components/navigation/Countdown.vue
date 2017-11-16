50dfb6a02f68a27df676d10616fd4fcf2d11f38a
Themis Beris <themis.n.beris@gmail.com>
2017-11-16 18:11:16 +0200

    Remove blockchain countdown

--- n3-composer/components/navigation/Countdown.vue
+++ n3-composer/components/navigation/Countdown.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <div v-if="blockchainCommitObj">
-      <h3 class="has-text-centered subtitle"><b>Blockchain Status</b>: {{blockchainCommitObj | formatDate}}</h3>
       <h4 class="has-text-centered subtitle is-6" style="padding-bottom:2em"><b>Τελευταίο Commit στο Blockchain</b>: <a :href="getLastBlockchainCommitLink(blockchainCommitObj)" target="_blank">{{blockchainCommitObj.txId}}</a> (<a href="/api/getLastMerkleTree" target="_blank">Merkle Tree</a>)</h4>
     </div>
     <div v-else>
@@ -20,22 +19,6 @@ export default {
   methods: {
     getLastBlockchainCommitLink: function (blockchainCommitObj) {
       return 'https://chain.so/tx/BTCTEST/' + blockchainCommitObj.txId
-    }
-  },
-  filters: {
-    formatDate: function (blockchainCommitObj) {
-      if (blockchainCommitObj.lastCommit && blockchainCommitObj.blockchainPoBMinutes) {
-        let lastCommitUnix = moment(String(blockchainCommitObj.lastCommit)).unix()
-        let currentTime = moment().unix()
-        let timeDifference = blockchainCommitObj.blockchainPoBMinutes - parseInt((currentTime - lastCommitUnix) / 60)
-        if (timeDifference < 0) {
-          return 'Σε αναμονή για commit στο Blockchain'
-        } else if (timeDifference == 1) {
-          return 'Απομένει περίπου 1 λεπτό για commit στο Blockchain'
-        } else {
-          return 'Απομένουν ' + timeDifference + ' λεπτά για commit στο Blockchain'
-        }
-      }
     }
   }
 }
