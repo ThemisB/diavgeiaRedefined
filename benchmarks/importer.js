dbba079713ffb6fabdef7ad280afacdd06fed2cd
Themis Beris <themis.n.beris@gmail.com>
2017-11-30 12:48:07 +0200

    export import function and use async to insert generated decisions

--- benchmarks/importer.js
+++ benchmarks/importer.js
@@ -1,27 +1,30 @@
-const randomDecision = require('./random-decision.js')
-const request = require('request')
-const async = require('async')
-const LOCALHOST = 'http://localhost:3000'
-const DATA_SAMPLES = 1000
+exports.import = (cb) => {
+  const randomDecision = require('./random-decision.js')
+  const request = require('request')
+  const async = require('async')
+  const LOCALHOST = 'http://localhost:3000'
+  const PER_DAY_DECISIONS = 13000
 
-const randomDecisions = []
-for (let i = 0; i < DATA_SAMPLES; i++) {
-  randomDecisions.push(randomDecision.generate())
-}
-let randomDecisionInsertion = (decision) => {
-  return new Promise((resolve, reject) => {
-    request.post(LOCALHOST + '/api/createDecisionBenchmark', {form: decision}, function (err, httpResponse, data) {
-      if (err) return reject(err)
-      let result = JSON.parse(data)
-      if (result !== 'OK') return reject(err)
-      resolve(result)
+  const randomDecisions = []
+  for (let i = 0; i < PER_DAY_DECISIONS; i++) {
+    randomDecisions.push(randomDecision.generate())
+  }
+  let randomDecisionInsertion = (decision) => {
+    return new Promise((resolve, reject) => {
+      request.post(LOCALHOST + '/api/createDecisionBenchmark', {form: decision}, function (err, httpResponse, data) {
+        if (err) return reject(err)
+        let result = JSON.parse(data)
+        if (result !== 'OK') return reject(err)
+        resolve(result)
+      })
     })
+  }
+
+  async.mapLimit(randomDecisions, 5, async function (decision) {
+    let result = await randomDecisionInsertion(decision)
+    return result
+  }, (err, result) => {
+    if (err) throw err
+    cb(result)
   })
 }
-
-async.mapLimit(randomDecisions, 5, async function (decision) {
-  let result = await randomDecisionInsertion(decision)
-  return result
-}, (err, result) => {
-  if (err) throw err
-})
