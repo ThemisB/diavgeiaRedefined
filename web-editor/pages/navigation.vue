7f7be5b51c6956ab4511dd72cd1f3f437db56253
Themis Beris <themis.n.beris@gmail.com>
2018-02-13 16:41:30 +0200

    Merge Decision.vue to navigation.vue

--- web-editor/pages/navigation.vue
+++ web-editor/pages/navigation.vue
@@ -32,7 +32,27 @@
     <div class="container">
       <countdown v-bind:blockchainCommitObj="blockchainCommitObj"></countdown>
       <div class="columns" v-for="i in Math.ceil(decisions.length / 3)">
-        <decision v-for="decision in decisions.slice((i-1) * 3, i * 3)" v-bind:decision="decision" :key="getDecisionsCounter()"></decision>
+        <div v-for="decision in decisions.slice((i-1) * 3, i * 3)" class="column is-one-third">
+          <div class="card">
+            <div class="card-content">
+                <p class="title is-4 has-text-centered" style="padding-bottom:0.3em">{{decision.title}}</p>
+                <p class="subtitle is-6">
+                  <div style="padding-bottom:0.3em"><span class="tag">ΑΔΑ</span>:&nbsp;{{decision.iun}} </div>
+                  <div style="padding-bottom:0.3em"><span class="tag"> Version</span>:&nbsp;{{decision.version}} </div>
+                  <div><span class="tag">Ημερομηνία ανάρτησης</span>:&nbsp; <time>{{decision.date | formatDate}}</time></div>
+                  <div class="has-text-centered" style="padding-top:1em" v-if="decision.txIndex || decision.txIndex === 0"><i class="fa fa-btc btcColor"></i><span style="font-size:1.1em"><b>-secured</b></span></div>
+                </p>
+            </div>
+            <footer class="card-footer">
+              <p class="card-footer-item">
+                <a :href="visualiseLink(decision)" target="_blank">Oπτικοποίηση</a>
+              </p>
+              <p class="card-footer-item">
+                <span><a :href="getDownloadLink(decision)">Κατεβάστε την απόφαση</a></span>
+              </p>
+            </footer>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -42,8 +62,8 @@
 <script>
 
 import axios from 'axios'
-import Decision from '../components/navigation/Decision.vue'
 import Countdown from '../components/navigation/Countdown.vue'
+import moment from 'moment'
 
 export default {
   head() {
@@ -60,6 +80,19 @@ export default {
     getDecisionsCounter: function () {
       this.decisionsCounter++
       return 'decision_' + this.decisionsCounter
+    },
+    visualiseLink: function (decision) {
+      return 'http://localhost:3333/visualize?iun=' + decision.iun + '&version=' + decision.version
+    },
+    getDownloadLink: function (decision) {
+      return '/api/downloadDecision?iun=' + decision.iun + '&version=' + decision.version
+    }
+  },
+  filters: {
+    formatDate: function (value) {
+      if (value) {
+        return moment(String(value)).format('DD/MM/YYYY, hh:mm')
+      }
     }
   },
   async asyncData({app}) {
